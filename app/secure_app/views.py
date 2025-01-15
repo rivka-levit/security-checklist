bd16fc9c9ca6b6dec220a598c2a53da22e1c62e6
rivka-yuliia <juliacreadora@gmail.com>
2025-01-15 17:41:34 +0200

    secure_app templates

--- app/secure_app/views.py
+++ app/secure_app/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render
+
 from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -5,15 +7,29 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Secure Checklist'
+        return context
+
 
 class RegisterView(View):
-    pass
+    def get(self, request):
+        context = {'title': 'Secure Checklist | Register'}
+        return render(request, 'register.html', context)
 
 
 class LoginView(View):
-    pass
+    def get(self, request):
+        context = {'title': 'Secure Checklist | Login'}
+        return render(request, 'login.html', context)
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     login_url = 'login'
     template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Secure Checklist | Dashboard'
+        return context
