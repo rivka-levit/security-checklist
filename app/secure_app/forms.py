33542658dea287bb9c8519aebf51fffd7a5a5534
rivka-yuliia <juliacreadora@gmail.com>
2025-01-22 18:07:38 +0200

    recaptcha to template

--- app/secure_app/forms.py
+++ app/secure_app/forms.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from django_recaptcha.fields import ReCaptchaField
+
 
 class CreateUserForm(UserCreationForm):
     """User creation form."""
+
+    captcha = ReCaptchaField()
 
     class Meta:
         model = User
